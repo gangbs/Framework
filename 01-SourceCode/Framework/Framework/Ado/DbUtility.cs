ca06d35dd998ec57ece2f822bf9324611d0806d9
gangbs <yangyangangil@163.com>
2018-10-22 21:59:55 +0800

    数据库连接生成方法封装

---
+++ 01-SourceCode/Framework/Framework/Ado/DbUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Ado
+{
+   public class DbUtility
+    {
+        public static DbConnection CreateConnection(string providerName,string strConn)
+        {
+            //string providerName = ConfigurationManager.ConnectionStrings["DbConnection"].ProviderName;
+            //string strConn = ConfigurationManager.ConnectionStrings["DbConnection"].ConnectionString;
+            DbProviderFactory factory = DbProviderFactories.GetFactory(providerName);
+            DbConnection conn = factory.CreateConnection();
+            conn.ConnectionString = strConn;
+            return conn;
+        }
+    }
+}
