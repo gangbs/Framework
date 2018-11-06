c8108ff4ba03f7270c5b3b7005e04184c798a932
gangbs <yangyangangil@163.com>
2018-11-06 23:11:49 +0800

    增加ajax返回实体

---
+++ 01-SourceCode/Framework/Framework/MVC/AjaxWebResponse.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public class AjaxWebResponse
+    {
+        public AjaxWebResponse(bool isSuccess)
+        {
+            this.success = isSuccess;
+        }
+
+        public bool success { get; set; }
+
+        public object result { get; set; }
+
+        public AjaxErrorWebResponse error { get; set; }
+
+        public string targetUrl { get; set; }
+
+        public bool unAuthorizedRequest { get; set; }
+    }
+
+
+    public class AjaxWebResponse<T> where T : class
+    {
+        public AjaxWebResponse(bool isSuccess)
+        {
+            this.success = isSuccess;
+        }
+
+        public bool success { get; set; }
+
+        public T result { get; set; }
+
+        public AjaxErrorWebResponse error { get; set; }
+
+        public string targetUrl{get;set;}
+
+        public bool unAuthorizedRequest { get; set; }
+    }
+
+
+    public class AjaxErrorWebResponse
+    {
+        public string message { get; set; }
+
+        public object detail { get; set; }
+    }
+
+}
---
+++ 01-SourceCode/Framework/Framework/MVC/MvcBaseController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Framework
+{
+   public class MvcBaseController:Controller
+    {
+        public AjaxWebResponse CreateSuccessResonse()
+        {
+            var res = new AjaxWebResponse(true);
+            return res;
+        }
+
+        public AjaxWebResponse CreateSuccessResonse<T>(T data)
+        {
+            var res = new AjaxWebResponse(true)
+            {
+                 result=data
+            };
+            return res;
+        }
+
+        public AjaxWebResponse CreateErrorResonse()
+        {
+            var res = new AjaxWebResponse(false);
+            return res;
+        }
+
+        public AjaxWebResponse CreateErrorResonse(AjaxErrorWebResponse errorInfo)
+        {
+            var res = new AjaxWebResponse(false)
+            {
+                error = errorInfo
+            };
+            return res;
+        }
+
+        public AjaxWebResponse CreateUnAuthorizeResonse(string url=null)
+        {
+            var res = new AjaxWebResponse(false)
+            {
+                 unAuthorizedRequest=true,
+                 targetUrl=url
+            };
+            return res;
+        }
+    }
+}
