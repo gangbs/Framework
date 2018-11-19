95b967f251bc6ceb9b2ae5b793b408c242ac27b3
杨彦钢 <yangyangangil@hotmail.com>
2018-11-19 19:59:15 +0800

    xml序列化扩展方法添加

---
+++ 01-SourceCode/Framework/Framework/Extension/ClaimsIdentityExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+    public static class ClaimsIdentityExtension
+    {
+        public static T ToUserInfo<T>(this ClaimsIdentity claimsIdentity) where T : new()
+        {
+            Type tp = typeof(T);
+            var constructor = tp.GetConstructor(Type.EmptyTypes);
+            T user = (T)constructor.Invoke(new Object[0]);
+
+            foreach (var item in tp.GetProperties())
+            {
+                var claim = claimsIdentity.FindFirst(item.Name);
+                if (claim == null) continue;
+                item.SetValue(user, Convert.ChangeType(claim.Value, item.PropertyType));
+            }
+            return user;
+        }
+    }
+}
---
+++ 01-SourceCode/Framework/Framework/Extension/XmlExtension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Framework
+{
+    public static class XmlExtension
+    {
+        /// <summary>
+        /// 对象Xml序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string ToXml<T>(this T obj) where T : class
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                xmlSerializer.Serialize(stream, obj);
+                stream.Position = 0;
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    string xml = sr.ReadToEnd();
+                    return xml;
+                }
+            }
+        }
+
+        /// <summary>
+        /// xml反序列化为对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static T ToObject<T>(this string xml) where T : class
+        {
+            using (StringReader sr = new StringReader(xml))
+            {
+                XmlSerializer s = new XmlSerializer(typeof(T));
+                return s.Deserialize(sr) as T;
+            }
+        }
+    }
+}
