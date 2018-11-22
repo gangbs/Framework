f73628abc24c2cc3961e8264163554c3ffbdc943
gangbs <yangyangangil@163.com>
2018-11-22 22:57:12 +0800

    新增IQueryable扩展方法

---
+++ 01-SourceCode/Framework/Framework/Extension/QueryableExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework
+{
+   public static class QueryableExtension
+    {
+        public static IQueryable<T> Paging<T>(this IOrderedQueryable<T> exp, int pageIndex, int pageSize)
+        {
+            return exp.Skip((pageIndex - 1) * pageSize).Take(pageSize); 
+        }
+
+        public static IQueryable<T> Paging<T,K>(this IQueryable<T> exp, Expression<Func<T, K>> orderFiled, int pageIndex, int pageSize,bool isAsc)
+        {
+            if(isAsc)
+            {
+                return exp.OrderBy(orderFiled).Skip((pageIndex - 1) * pageSize).Take(pageSize); 
+            }
+            else
+            {
+                return exp.OrderByDescending(orderFiled).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            }
+        }
+    }
+}
